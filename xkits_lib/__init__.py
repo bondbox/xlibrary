29ab13dd1ebac7adc2626c2d41fbcdd69cf2fee7
Mingzhe Zou <zoumingzhe@qq.com>
2025-11-04 15:53:26 +0800

    remove all imports

--- xkits_lib/__init__.py
+++ xkits_lib/__init__.py
@@ -1,22 +1,3 @@
-# coding:utf-8
-
-from xkits_lib.cache import CacheAtom  # noqa:F401
-from xkits_lib.cache import CacheData  # noqa:F401
-from xkits_lib.cache import CacheExpired  # noqa:F401
-from xkits_lib.cache import CacheItem  # noqa:F401
-from xkits_lib.cache import CacheLookupError  # noqa:F401
-from xkits_lib.cache import CacheMiss  # noqa:F401
-from xkits_lib.cache import CachePool  # noqa:F401
-from xkits_lib.cache import ItemPool  # noqa:F401
-from xkits_lib.cache import NamedCache  # noqa:F401
-from xkits_lib.meter import CountMeter  # noqa:F401
-from xkits_lib.meter import DownMeter  # noqa:F401
-from xkits_lib.meter import LiveMeter  # noqa:F401
-from xkits_lib.meter import StatusCountMeter  # noqa:F401
-from xkits_lib.meter import TimeMeter  # noqa:F401
-from xkits_lib.meter import TsCountMeter  # noqa:F401
-from xkits_lib.path import Workspace  # noqa:F401
-from xkits_lib.time import Timestamp  # noqa:F401
-from xkits_lib.unit import DataUnit  # noqa:F401
-from xkits_lib.unit import TimeUnit  # noqa:F401
-from xkits_lib.utils import singleton  # noqa:F401
+# The xlibrary is a common module, requires minimal memory overhead.
+# Do not import anything here to avoid unnecessary memory usage.
+# Users can import as needed.
